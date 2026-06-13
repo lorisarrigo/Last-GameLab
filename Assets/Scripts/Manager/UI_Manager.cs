24866af1bd190155d3606c4bb736459a42d87a59
Loris Arrigo <loris.arrigo@gmail.com>
2026-06-13 18:59:22 +0100

    basic NPC movement
    - implementato Npc che si muove in base se diamo o meno la brochure

--- Assets/Scripts/Manager/UI_Manager.cs
+++ Assets/Scripts/Manager/UI_Manager.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,12 +9,18 @@ public class UI_Manager : MonoBehaviour
     public int Goal;
     [Header("patience")]
     [SerializeField] float patienceTimer;
-    public float elapsed = 0;
-    public bool isFilling = false;
+    float elapsed = 0;
+    bool isFilling = false;
+    public bool success;
     [SerializeField] Image patienceBar;
     [SerializeField] Image patienceBarFB;
     [Header("Score")]
     [SerializeField] TMP_Text score;
+
+
+    //eventi
+    public static event Action OnFinishedTimer;
+    public static event Action OnDeliver;
 
     public static UI_Manager instance;
 
@@ -29,27 +36,43 @@ public class UI_Manager : MonoBehaviour
     private void OnEnable()
     {
         Game_Manager.OnPoint += UpdateGoal;
+        NPC_Manager.OnTimer += StartTimer;
     }
     private void OnDisable()
     {
         Game_Manager.OnPoint -= UpdateGoal;
-    }
-    private void Start()
-    {
-        elapsed = patienceTimer;
+        NPC_Manager.OnTimer -= StartTimer;
     }
     private void Update()
     {
         if (isFilling && elapsed > 0)
         {
+            if(success)
+            {
+                OnDeliver?.Invoke();
+                patienceBar.gameObject.SetActive(false);
+                return;
+            }
             elapsed -= Time.deltaTime;
             patienceBar.fillAmount = elapsed / patienceTimer;
             patienceBarFB.fillAmount = elapsed / patienceTimer;
             if (elapsed <= 0)
+            {
                 isFilling = false;
+                OnFinishedTimer?.Invoke();
+            }
         }
     }
-    private void UpdateGoal()
+    void StartTimer()
+    {
+        if (!isFilling)
+        {
+            patienceBar.gameObject.SetActive(true);
+            elapsed = patienceTimer;
+        }
+        isFilling = true;
+    }
+    void UpdateGoal()
     {
         //questo serve per il minigame
         score.text = currentScore + "/" + Goal;
