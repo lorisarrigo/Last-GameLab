dce101866029549608f344220bbe374acbce68d0
giu <giuliocalo05@gmail.com>
2026-07-10 02:38:35 +0200

    GD push
    new aliens +  fixed menu + GM code updated + idk more prolly

--- Assets/Scripts/Manager/Game_Manager.cs
+++ Assets/Scripts/Manager/Game_Manager.cs
@@ -89,7 +89,7 @@ public class Game_Manager : MonoBehaviour
         {
             NPC_Manager.instance.clientLeft = 1;
             Jew_Manager.instance.currentMoney = Jew_Manager.instance.overallTotal;
-            UI_Manager.instance.moneyCounter.text = Jew_Manager.instance.currentMoney + " Æ";
+            UI_Manager.instance.moneyCounter.text = Jew_Manager.instance.currentMoney + " ï¿½";
 
             if (Save_Manager.instance != null)
             {
@@ -137,6 +137,13 @@ public class Game_Manager : MonoBehaviour
             case 7: FB_Manager.instance.Goal = FB_Manager.instance.goal1; break;
             case 8: FB_Manager.instance.Goal = FB_Manager.instance.goal2; break;
             case 9: FB_Manager.instance.Goal = FB_Manager.instance.goal3; break;
+            case 16: FB_Manager.instance.Goal = FB_Manager.instance.goal1; break;k;
+            case 13: FB_Manager.instance.Goal = FB_Manager.instance.goal1; break;
+            case 12: FB_Manager.instance.Goal = FB_Manager.instance.goal2; break;
+            case 15: FB_Manager.instance.Goal = FB_Manager.instance.goal3; break;
+            case 17: FB_Manager.instance.Goal = FB_Manager.instance.goal1; break;
+            case 18: FB_Manager.instance.Goal = FB_Manager.instance.goal2; break;
+            case 19: FB_Manager.instance.Goal = FB_Manager.instance.goal3; break;
         }
         OnPoint?.Invoke();
     }
